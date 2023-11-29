26d89e815c14d0a5399b70dd001e8c259f1258f5
dgnarslan <arslan.dgn51@gmail.com>
2023-11-29 22:43:18 +0300

    New file added

--- src/components/form/File.js
+++ src/components/form/File.js
@@ -1,16 +1,27 @@
 import {useField} from "formik";
+import {FiCheck} from "react-icons/fi";
 
-export default function File({label, ...props}){
-    const [field,meta,helpers]= useField(props)
+export default function File({label, ...props}) {
+    const [field, meta, helpers] = useField(props)
 
-    const changeHandle = e => {
-        helpers.setValue(e.target.files[0])
+    async function fileOpen() {
+        try {
+            const [fileHandle] = await window.showOpenFilePicker()
+            const file = await fileHandle.getFile()
+            helpers.setValue(file)
+        } catch (e) {
+            helpers.setValue('')
+        }
     }
-    return(
+
+    return (
         <label className='block w-full'>
             <div className='text-sm text-gray-600'>{label}</div>
-            <input className='hidden' type={'file'} onChange={changeHandle} {...props} />
-            <button className='px-5 pointer-events-none rounded bg-blue-50 h-10 text-blue-500'> Dosya seç</button>
+
+            <button type={'button'} onClick={fileOpen} className='px-5 text-sm rounded bg-blue-50 h-10 text-blue-500'>
+                {field.value && <>Dosya seçildi <FiCheck size={15}/></>}
+                {!field.value && 'Dosya Seç'}
+            </button>
         </label>
     )
 }
